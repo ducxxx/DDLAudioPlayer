512d4238676252cfb0c0bcd994f992df996e7419
ducxxx <ilovefcbclub@gmail.com>
2023-03-21 01:16:32 +0700

    sprin1

--- app/src/main/java/com/example/ddlaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/ddlaudioplayer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ddlaudioplayer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -53,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return fragments.size();
+        }
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position){
+            return titles.get(position);
         }
     }
 }
